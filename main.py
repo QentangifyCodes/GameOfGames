7fb6db7b0057f653191f5a8d14c1656787956e51
Sharul <sharulshah@icloud.com>
2022-05-13 13:29:44 -0500

    Added some text

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-#Gamer part 2
-def typeOut(message: str, speed: float = 0.000001):
-    print(end="\n")
-    i = 0
-    u = 0
-    while u < len(message):
-        i += speed
-        if int(i) > 1:
-            i = 0
-            print(message[u], end="")
-            u += 1
-    del i, u
 
-
-typeOut("This is a test program")
-typeOut("We are using pygame now")
-
-while True:
-    s = input("\nEnter something to type out. Type 'end' to end: ")
-    if s.lower() == "end":
-        print("Stopping program")
-        break
-    typeOut("[You]: "+ s)
-    del s
+print("This is a test program")
+print("We are using pygame now")
+print("Who asked")
