35bb6235630a8722be9e048603bf3ce2c7db90ba
Keshav <keshavrshah@gmail.com>
2022-05-13 13:53:52 -0500

    Basic Pygame Window

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import pygame
+pygame.init()
 
-print("This is a test program")
-print("We are using pygame now")
-print("Who asked")
-#noIDidNotAsk
+screen = pygame.display.set_mode((1000, 700))
+pygame.display.set_caption('Placeholder')
+
+running = True
+while running:
+    screen.fill((23, 23, 23))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
